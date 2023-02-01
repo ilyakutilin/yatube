c06ccbc4a5fd07d39ccfc8f3ee5f91269f285c10
Ilya Kutilin <ilyakutilin@yandex.ru>
2023-02-01 09:21:59 +0300

    Correct Email Backend
    - Include DEFAULE_FROM_EMAIL setting so that the originator Email
      matches the Email Host
    - Move 'Use TLS' and 'Use SSL' settings to env variables

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -119,8 +119,9 @@ EMAIL_HOST = os.getenv('EMAIL_HOST')
 EMAIL_PORT = os.getenv('EMAIL_PORT')
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
-EMAIL_USE_TLS = True
-EMAIL_USE_SSL = False
+EMAIL_USE_TLS = (os.getenv('EMAIL_USE_TLS', 'False') == 'True')
+EMAIL_USE_SSL = (os.getenv('EMAIL_USE_SSL', 'False') == 'True')
+DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
 
 CSRF_FAILURE_VIEW = 'core.views.csrf_failure'
 
